62debe218e72f76547fc1a2d7fbb7050c4c8c147
Yashraj Jhala <jhalayashraj@gmail.com>
2016-07-18 21:43:05 +0530

    Let Google keyboard be the part

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_mini.mk)
 
-# Required CM packages
-PRODUCT_PACKAGES += \
-    LatinIME
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
