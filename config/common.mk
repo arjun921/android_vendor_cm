72abf74d52319bdc421ca05b3c7a1206961bfb99
Yashraj Jhala <jhalayashraj@gmail.com>
2016-11-30 00:54:50 +0530

    Add back ViPER4Android

--- config/common.mk
+++ config/common.mk
@@ -122,6 +122,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.local.rc:root/init.cm.rc
 
+# LegendROM apps
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/app/Viper4Android/Viper4Android.apk:/system/app/Viper4Android/Viper4Android.apk
+
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
