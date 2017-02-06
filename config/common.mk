305f2d0cce7779b9eb692912299e7fc0cf06a274
Yashraj Jhala <jhalayashraj@gmail.com>
2017-02-06 12:55:15 +0000

    12.0
    Change-Id: Ia8dce749cc3407f2f3114d487d37ce9714ee1cdf

--- config/common.mk
+++ config/common.mk
@@ -334,9 +334,9 @@ ifeq ($(CM_BUILDTYPE), RELEASE)
     endif
 else
     ifeq ($(CM_VERSION_MAINTENANCE),0)
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-LegendROM-11.0$(CM_EXTRAVERSION)-$(CM_BUILD)
+        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-LegendROM-12.0$(CM_EXTRAVERSION)-$(CM_BUILD)
     else
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(CM_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-LegendROM-11.0$(CM_EXTRAVERSION)-$(CM_BUILD)
+        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(CM_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-LegendROM-12.0$(CM_EXTRAVERSION)-$(CM_BUILD)
     endif
 endif
 
@@ -378,7 +378,7 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(CM_DISPLAY_VERSION) \
-  ro.legendrom.version=11.0
+  ro.legendrom.version=12.0
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/cm/config/partner_gms.mk
