3d306cd3fc38f00eee81d2fd65a2d9cd23d56a71
nathanchance <natechancellor@gmail.com>
2017-03-09 05:19:06 +1100

    Switch to Magisk as the built in root [1/2]
    Change-Id: If6ea61446bd7b44811970a292ac232d04f775584
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -171,6 +171,14 @@ PRODUCT_PACKAGES += \
     WeatherProvider \
     ThemeInterfacer
 
+# Magisk Manager
+PRODUCT_PACKAGES += \
+    MagiskManager
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/magisk.zip:system/addon.d/magisk.zip
+
 # Exchange support
 PRODUCT_PACKAGES += \
     Exchange2
