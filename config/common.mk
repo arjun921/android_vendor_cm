60fffc771d6ffbbfb03c5cb375293fef1ea73ff6
Yashraj Jhala <jhalayashraj@gmail.com>
2017-07-18 14:27:33 +0000

    Vendor: Update Magisk and MagiskManager to Latest
    Change-Id: Icc1498d997150ae25c47a6e247442b90618b9824

--- config/common.mk
+++ config/common.mk
@@ -353,9 +353,9 @@ ifeq ($(CM_BUILDTYPE), RELEASE)
     endif
 else
     ifeq ($(CM_VERSION_MAINTENANCE),0)
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-LegendROM-17.0$(CM_EXTRAVERSION)-$(CM_BUILD)
+        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-LegendROM-18.0$(CM_EXTRAVERSION)-$(CM_BUILD)
     else
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(CM_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-LegendROM-17.0$(CM_EXTRAVERSION)-$(CM_BUILD)
+        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(CM_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-LegendROM-18.0$(CM_EXTRAVERSION)-$(CM_BUILD)
     endif
 endif
 
@@ -397,7 +397,7 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(CM_DISPLAY_VERSION) \
-  ro.legendrom.version=17.0
+  ro.legendrom.version=18.0
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/cm/config/partner_gms.mk
